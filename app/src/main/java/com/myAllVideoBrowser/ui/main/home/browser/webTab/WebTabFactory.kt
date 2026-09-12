d2fbeda6871d37522c91e8366b7586ed91d57519
finchcode-sudo <poqq26566@gmail.com>
2026-09-12 23:11:15 +0800

    Update WebTabFactory.kt

--- app/src/main/java/com/myAllVideoBrowser/ui/main/home/browser/webTab/WebTabFactory.kt
+++ app/src/main/java/com/myAllVideoBrowser/ui/main/home/browser/webTab/WebTabFactory.kt
@@ -2,6 +2,7 @@ package com.myAllVideoBrowser.ui.main.home.browser.webTab
 
 import android.util.Patterns
 import com.myAllVideoBrowser.ui.main.home.browser.BrowserViewModel
+import com.myAllVideoBrowser.ui.main.settings.SearchEngine
 
 class WebTabFactory {
     companion object {
@@ -12,8 +13,11 @@ class WebTabFactory {
                 } else if (Patterns.WEB_URL.matcher(input).matches()) {
                     WebTab("https://$input", null, null, emptyMap())
                 } else {
+                    val searchUrlTemplate =
+                        BrowserViewModel.instance?.settingsModel?.selectedSearchEngine?.get()?.searchUrl
+                            ?: SearchEngine.DUCKDUCKGO.searchUrl
                     WebTab(
-                        String.format(BrowserViewModel.SEARCH_URL, input),
+                        String.format(searchUrlTemplate, input),
                         null,
                         null,
                         emptyMap())
